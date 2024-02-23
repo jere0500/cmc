e5439df168a9747ada8021759346e5968862cacf
Jeremias Giesecke <Jeremias.Giesecke@aisec.fraunhofer.de>
2024-02-23 10:16:50 +0100

    [PRE]: attestedtls: implemented Conn
    Signed-off-by: Jeremias Giesecke <Jeremias.Giesecke@aisec.fraunhofer.de>

--- attestedtls/conn.go
+++ attestedtls/conn.go
@@ -19,6 +19,7 @@ import (
 	"bytes"
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -40,14 +41,32 @@ type Conn struct {
 
 	//?maybe additional attributs
 	//?the two timer layouts
-
-	//stores the current state of reading the magic value
-	magicReadingState int
 }
 
-func (c *Conn) Write(b []byte) (int, error) {
-	//? TODO: handling of time out timer,...
-	out, err := c.Conn.Write(b)
+// Write implements net.Conn.
+func (c *Conn) Write(b []byte) (n int, err error) {
+	if c.lastAttestation.Second()+REATTEST_AFTER_TIME+GRACE_PERIOD > time.Now().Second() {
+		return -1, fmt.Errorf("no reattestation received, terminate connnection")
+	}
+
+	if c.lastAttestation.Second()+REATTEST_AFTER_TIME > time.Now().Second() {
+		//do reattestation if (necessary)
+		initiateReattest(c)
+		if err != nil {
+			//still forwarding the byte
+			log.Debug(err)
+		}
+	}
+
+	return c.Conn.Write(b)
+}
+
+func (c *Conn) Read(b []byte) (int, error) {
+	//? TODO: check if the timing of the package is still in frame, or not, ..., other checks
+	out, err := c.Conn.Read(b)
+	if c.lastAttestation.Second()+REATTEST_AFTER_TIME+GRACE_PERIOD > time.Now().Second() {
+		return -1, fmt.Errorf("no reattestation received, terminate connnection")
+	}
 
 	if c.lastAttestation.Second()+REATTEST_AFTER_TIME > time.Now().Second() {
 		//do reattestation if (necessary)
@@ -56,6 +75,32 @@ func (c *Conn) Write(b []byte) (int, error) {
 			//still forwarding the byte
 			return out, err
 		}
+	}
+
+	//check for any incomming attestation reports
+	ret := bytes.Index(b, ATLS_MAGIC_VALUE[:])
+	if ret != -1 {
+		// buffer contains the magic value
+		report, err := readValue(b[ret:], c.cc.Attest, c.isDialer, c.cc)
+		if err != nil {
+			if strings.Contains("could not unmarshal atls response", err.Error()) {
+				//invalid cbor serialization
+				//continue
+				log.Trace(err)
+			} else {
+				//? should probably terminate the connection
+				log.Tracef("invalid attestation report: %v", err)
+				return -1, err
+			}
+		} else {
+			//verify the attestation report
+			validateAttestationReport(c.chbindings, c.cc, report, c.isDialer)
+
+			//sucessfull received and validated attestation report
+			c.lastAttestation = time.Now()
+			c.sentReattest = false
+		}
+
 	}
 
 	return out, err
@@ -68,7 +113,7 @@ func initiateReattest(c *Conn) error {
 	}
 
 	//do reattestation if (necessary)
-	err := sendAttestationReport(c, c.chbindings, c.cc, c.isDialer)
+	err := sendAttestationReport(c.Conn, c.chbindings, c.cc, c.isDialer)
 	if err != nil {
 		//still forwarding the byte
 		return err
@@ -76,37 +121,6 @@ func initiateReattest(c *Conn) error {
 	c.sentReattest = true
 
 	return nil
-}
-
-func (c *Conn) Read(b []byte) (int, error) {
-	//? TODO: check if the timing of the package is still in frame, or not, ..., other checks
-	out, err := c.Conn.Read(b)
-	if c.lastAttestation.Second()+REATTEST_AFTER_TIME+GRACE_PERIOD > time.Now().Second() {
-		return -1, fmt.Errorf("no reattestation received")
-	}
-
-	if c.lastAttestation.Second()+REATTEST_AFTER_TIME > time.Now().Second() {
-		//do reattestation if (necessary)
-		initiateReattest(c)
-		if err != nil {
-			//still forwarding the byte
-			return out, err
-		}
-	}
-
-	//? TODO check for the next magic value
-	ret := bytes.Index(b, ATLS_MAGIC_VALUE[:])
-	if ret != -1 {
-		// buffer contains the magic value
-		retval, err := readValue(b[ret:], c.cc.Attest, c.isDialer, c.cc)
-		if err != nil {
-			return -1, err
-		}
-		//? TODO return the magic value
-
-	}
-
-	return out, err
 }
 
 // boilerplate code
